015e22a817f48625b2a2add580601d34238cc3f8
zontchan <semenovajuls@icloud.com>
2023-10-25 14:10:29 +0700

    ConfirmButton added styles and link to final page

--- src/components/ConfirmButton/ConfirmButton.js
+++ src/components/ConfirmButton/ConfirmButton.js
@@ -1,3 +1,11 @@
-export default function ConfirmButton() {
-    return (<div>Confirm button</div>);
+import styles from "./ConfirmButton.module.css";
+import {Link} from "react-router-dom";
+
+
+export default function ConfirmButton({isDisabled}) {
+    return  (
+        <Link to={'/final-info'} ><button className={isDisabled ? `${styles.confirmButton}` : `${styles.confirmButton} ${styles.active}`}
+                                          disabled={isDisabled}>
+            Подтвердить номер</button></Link>
+    );
 }
