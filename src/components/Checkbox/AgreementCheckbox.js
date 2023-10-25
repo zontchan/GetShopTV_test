a568542fc7b417cd1b5694cd93e141e6c4c2f69e
zontchan <semenovajuls@icloud.com>
2023-10-25 14:12:22 +0700

    Agreement checkbox added styles

--- src/components/Checkbox/AgreementCheckbox.js
+++ src/components/Checkbox/AgreementCheckbox.js
@@ -1,3 +1,13 @@
-export default function AgreementCheckbox() {
-    return (<div>Checkbox</div>);
+import styles from "./AgreementCheckbox.module.css";
+
+export default function AgreementCheckbox({isChecked}) {
+    return (
+        <div className={styles.agreement}>
+            <label className={styles.checkboxWrapper}>
+                <input type="checkbox" className={styles.agreementCheckbox} checked={isChecked}/>
+                <span></span>
+            </label>
+            <span className={styles.agreementText}>Согласие на обработку персональных данных</span>
+        </div>
+    );
 }
