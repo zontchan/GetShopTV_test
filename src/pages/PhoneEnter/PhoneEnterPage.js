a7736d5f020153105bf87ce5f83633a7b2ffabe9
zontchan <semenovajuls@icloud.com>
2023-10-25 14:54:56 +0700

    Added handlers on actions on arrow keys

--- src/pages/PhoneEnter/PhoneEnterPage.js
+++ src/pages/PhoneEnter/PhoneEnterPage.js
@@ -1,19 +1,18 @@
-import styles from './PhoneEnterPage.module.css';
-import SidePanelScreen from "../../components/SidePanelScreen/SidePanelScreen";
-import SidePanel from "../../components/SidePanel/SidePanel";
-import PhoneForm from "../../components/PhoneForm/PhoneForm";
-import CloseButton from "../../components/CloseButton/CloseButton";
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
+import styles from './PhoneEnterPage.module.css';
+import PhoneForm from "../../components/PhoneForm/PhoneForm";
+import SidePanel from "../../components/SidePanel/SidePanel";
+import SidePanelScreen from "../../components/SidePanelScreen/SidePanelScreen";
+import {setActiveKey, arrowUpAction, arrowDownAction, arrowLeftAction, arrowRightAction} from "../../reducers/keyboardReducer";
 import {addNumber, deleteNumber} from "../../reducers/phoneNumberReducer";
 import {setIsChecked} from "../../reducers/checkboxReducer";
-import {setActiveKey} from "../../reducers/keyboardReducer";
+import CloseButton from "../../components/CloseButton/CloseButton";
 
 export default function PhoneEnterPage() {
     const {activeKey, category} = useSelector((state) => state.keyboard);
     const phone =useSelector((state) => state.phoneNumber);
     const dispatch = useDispatch();
-
 
 
     const handleKeyDown =(e) => {
@@ -38,14 +37,18 @@ export default function PhoneEnterPage() {
             dispatch(setActiveKey({activeKey: e.key, category: 'keyboard'}))
             dispatch(addNumber(e.key));
         }
+        if(e.key === 'ArrowUp') dispatch(arrowUpAction());
+        if(e.key === 'ArrowDown') dispatch(arrowDownAction());
+        if(e.key === 'ArrowRight') dispatch(arrowRightAction());
+        if(e.key === 'ArrowLeft') dispatch(arrowLeftAction());
     }
-
     useEffect(() => {
         document.addEventListener('keydown', handleKeyDown);
         return () => {
             document.removeEventListener('keydown', handleKeyDown);
         };
     },[handleKeyDown]);
+
     return (
         <div className={styles.phoneEnterPage}>
             <SidePanelScreen>
