f5fdf2a8f636f0ed5b7d0b67f22fb0131da05dfb
zontchan <semenovajuls@icloud.com>
2023-10-25 14:43:09 +0700

    click handler on keyboard buttons, setClassName edited depending on active key

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -1,12 +1,33 @@
 import { v4 as uuidv4 } from 'uuid';
 import {keyboardValues} from '../../data/keyboardValues';
 import styles from './Keyboard.module.css';
+import {useDispatch, useSelector} from "react-redux";
+import {setActiveKey} from "../../reducers/keyboardReducer";
+import {addNumber, deleteNumber} from "../../reducers/phoneNumberReducer";
 
 export default function Keyboard() {
+    const {activeKey} = useSelector((state) => state.keyboard);
+    const phone =useSelector((state) => state.phoneNumber);
+    const dispatch = useDispatch();
+
+
+    const handleClick = (key) => {
+        dispatch(setActiveKey({activeKey: key, category: 'keyboard'}));
+        if(key === 'Remove' && phone.length === 0)  return;
+        if(key === 'Remove') return dispatch(deleteNumber())
+        if(phone.length === 10) return;
+        dispatch(addNumber(key));
+    }
 
     const setClassName= (key) => {
+        if(key === 'Remove' && activeKey === key){
+            return `${styles.keyBoardItem} ${styles.keyBoardItem_big} ${styles.keyBoardItem_active}`;
+        }
         if(key === 'Remove'){
             return `${styles.keyBoardItem} ${styles.keyBoardItem_big}`;
+        }
+        if(activeKey === key){
+            return `${styles.keyBoardItem} ${styles.keyBoardItem_active}`
         }
         return `${styles.keyBoardItem}`;
     }
@@ -16,6 +37,7 @@ export default function Keyboard() {
             {keyboardValues.map((key) =>
                 <button key={uuidv4()}
                         className={setClassName(key)}
+                        onClick={() => handleClick(key)}
                 >{key === 'Remove' ? 'Стереть' : key}</button>)}
         </div>
     );
