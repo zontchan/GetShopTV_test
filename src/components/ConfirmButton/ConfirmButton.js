57e06f4f6dd7574147679d42f4441c76e2616799
zontchan <semenovajuls@icloud.com>
2023-10-25 14:38:36 +0700

    Focus on confirm button depending on active key

--- src/components/ConfirmButton/ConfirmButton.js
+++ src/components/ConfirmButton/ConfirmButton.js
@@ -1,10 +1,22 @@
 import styles from "./ConfirmButton.module.css";
 import {Link} from "react-router-dom";
-
+import {useSelector} from "react-redux";
+import {useEffect, useRef} from "react";
 
 export default function ConfirmButton({isDisabled}) {
+    const {activeKey} = useSelector((state) => state.keyboard);
+    const ref = useRef(null);
+    useEffect(() => {
+        if(ref.current) {
+            if (activeKey === 'ConfirmButton') {
+                ref.current.focus();
+            } else ref.current.blur();
+        }
+    }, [activeKey]);
+
     return  (
         <Link to={'/final-info'} ><button className={isDisabled ? `${styles.confirmButton}` : `${styles.confirmButton} ${styles.active}`}
+                                          ref={ref}
                                           disabled={isDisabled}>
             Подтвердить номер</button></Link>
     );
