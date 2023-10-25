8059451bd049ac3b8e791899053c4d748f3c9189
zontchan <semenovajuls@icloud.com>
2023-10-25 14:14:42 +0700

    Keyboard styles added

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -1,3 +1,22 @@
+import { v4 as uuidv4 } from 'uuid';
+import {keyboardValues} from '../../data/keyboardValues';
+import styles from './Keyboard.module.css';
+
 export default function Keyboard() {
-    return (<div>Keyboard</div>);
+
+    const setClassName= (key) => {
+        if(key === 'Remove'){
+            return `${styles.keyBoardItem} ${styles.keyBoardItem_big}`;
+        }
+        return `${styles.keyBoardItem}`;
+    }
+
+    return (
+        <div className={styles.keyBoard}>
+            {keyboardValues.map((key) =>
+                <button key={uuidv4()}
+                        className={setClassName(key)}
+                >{key === 'Remove' ? 'Стереть' : key}</button>)}
+        </div>
+    );
 }
