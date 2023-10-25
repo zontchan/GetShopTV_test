f50775e28c7c6312d2d42bc86ca24919923966ff
zontchan <semenovajuls@icloud.com>
2023-10-25 14:36:35 +0700

    Checkbox styles connected with activeKey, dispatch onchange

--- src/components/Checkbox/AgreementCheckbox.js
+++ src/components/Checkbox/AgreementCheckbox.js
@@ -1,10 +1,16 @@
 import styles from "./AgreementCheckbox.module.css";
+import {useDispatch, useSelector} from "react-redux";
+import {setIsChecked} from "../../reducers/checkboxReducer";
 
-export default function AgreementCheckbox({isChecked}) {
+export default function AgreementCheckbox() {
+    const {isChecked}= useSelector((state) => state.checkbox);
+    const {activeKey} = useSelector((state) => state.keyboard);
+    const dispatch = useDispatch();
+
     return (
         <div className={styles.agreement}>
-            <label className={styles.checkboxWrapper}>
-                <input type="checkbox" className={styles.agreementCheckbox} checked={isChecked}/>
+            <label className={activeKey === 'Checkbox' ? `${styles.checkboxWrapper} ${styles.active}`:styles.checkboxWrapper}>
+                <input type="checkbox" className={styles.agreementCheckbox} checked={isChecked} onChange={() => dispatch(setIsChecked())}/>
                 <span></span>
             </label>
             <span className={styles.agreementText}>Согласие на обработку персональных данных</span>
