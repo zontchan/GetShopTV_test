50ccf6dc590dac7f4b0e40d52cb409b1a50b1765
zontchan <semenovajuls@icloud.com>
2023-10-25 13:54:16 +0700

    SidePaneScreen component done

--- src/components/SidePanelScreen/SidePanelScreen.js
+++ src/components/SidePanelScreen/SidePanelScreen.js
@@ -1,3 +1,14 @@
-export default function SidePanelScreen() {
-    return (<div>Side Panel Screen</div>);
+
+import styles from "./SidePanelScreen.module.css";
+
+export default function SidePanelScreen(props) {
+    return (
+        <div className={styles.promoScreen}>
+            {props.children}
+            <div className={styles.additionalInfo}>
+                <p className={styles.infoText}>Сканируйте QR-код для получения дополнительной информации</p>
+                <img src={'static/qr-code.png'} alt=""/>
+            </div>
+        </div>
+    );
 }
