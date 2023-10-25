2e7e6087d653c6951ae8b6c5052b3bcf5ed7a42b
zontchan <semenovajuls@icloud.com>
2023-10-25 14:57:08 +0700

    Focus on close button depending on active key

--- src/components/CloseButton/CloseButton.js
+++ src/components/CloseButton/CloseButton.js
@@ -1,10 +1,21 @@
 import styles from "./CloseButton.module.css";
 import {Link} from "react-router-dom";
-
+import {useSelector} from "react-redux";
+import {useEffect, useRef} from "react";
 
 export default function CloseButton({buttonStyle}) {
+    const {activeKey} = useSelector((state) => state.keyboard);
+    let ref = useRef(null);
+    useEffect(() => {
+        if(ref.current) {
+            if (activeKey === 'CloseButton') {
+                ref.current.focus();
+            } else ref.current.blur();
+        }
+    }, [activeKey, ref.current]);
+
     return (<Link to={'/'}>
-        <button className={buttonStyle === 'phoneEnterPageCloseButton' ? `${styles.closeButton} ${styles.phoneEnterPageCloseButton}` : `${styles.closeButton} ${styles.finalInfoPageCloseButton}`}>
+        <button  ref={ref} className={buttonStyle === 'phoneEnterPageCloseButton' ? `${styles.closeButton} ${styles.phoneEnterPageCloseButton}` : `${styles.closeButton} ${styles.finalInfoPageCloseButton}`}>
             <div className={styles.closeButtonContent}></div>
         </button>
     </Link>);
