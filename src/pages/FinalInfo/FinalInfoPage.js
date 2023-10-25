5e237ea344ef25358675473e6917aea435b0669d
zontchan <semenovajuls@icloud.com>
2023-10-25 15:13:00 +0700

    Final page done

--- src/pages/FinalInfo/FinalInfoPage.js
+++ src/pages/FinalInfo/FinalInfoPage.js
@@ -1,4 +1,19 @@
+import SidePanel from "../../components/SidePanel/SidePanel";
+import styles from './FinalInfoPage.module.css';
+import SidePanelScreen from "../../components/SidePanelScreen/SidePanelScreen";
+import CloseButton from "../../components/CloseButton/CloseButton";
 export default function FinalInfoPage() {
-
-    return (<div>Final Info Page</div>);
+    return (
+        <div className={styles.finalInfoPage}>
+            <SidePanelScreen>
+                <SidePanel>
+                    <div className={styles.sideContent}>
+                        <h1 className={styles.title}>Заявка принята</h1>
+                        <span className={styles.subtitle}>Держите телефон под рукой.<br/>Скоро с Вами свяжется наш менеджер.</span>
+                    </div>
+                    <CloseButton buttonStyle={'finalInfoPageCloseButton'}/>
+                </SidePanel>
+            </SidePanelScreen>
+        </div>
+    );
 }
