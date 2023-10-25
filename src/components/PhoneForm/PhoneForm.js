a9149835af452e0d34d3d78c967b30c5d45ab263
zontchan <semenovajuls@icloud.com>
2023-10-25 14:22:23 +0700

    Phone number mask done

--- src/components/PhoneForm/PhoneForm.js
+++ src/components/PhoneForm/PhoneForm.js
@@ -2,14 +2,25 @@ import styles from './PhoneForm.module.css';
 import KeyBoard from "../Keyboard/Keyboard"
 import AgreementCheckbox from "../Checkbox/AgreementCheckbox";
 import ConfirmButton from "../ConfirmButton/ConfirmButton";
+import {useSelector} from "react-redux";
+import {useEffect, useState} from "react";
 
 
 export default function PhoneForm() {
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const phone = useSelector((state) => state.phoneNumber);
+
+
+    useEffect(() => {
+        let template = '+7(___)___-__-__';
+        phone.forEach(i => template = template.replace(/_/, i));
+        setPhoneNumber(template);
+    },[phone]);
 
     return (
         <div className={styles.phoneForm}>
             <h3 className={styles.title}>Введите ваш номер мобильного телефона</h3>
-            <div className={styles.phoneNumber}>phone</div>
+            <div className={styles.phoneNumber}>{phoneNumber}</div>
             <p className={styles.subtitle}>и с Вами свяжется наш менеждер для дальнейшей консультации</p>
             <KeyBoard/>
             <AgreementCheckbox/>
