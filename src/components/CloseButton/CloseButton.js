211fa8961a8a61d51feb9634c032d5892cc7d22f
zontchan <semenovajuls@icloud.com>
2023-10-25 13:59:19 +0700

    Close button styles and link to start page

--- src/components/CloseButton/CloseButton.js
+++ src/components/CloseButton/CloseButton.js
@@ -1,3 +1,11 @@
-export default function CloseButton() {
-    return (<div>Close button</div>);
+import styles from "./CloseButton.module.css";
+import {Link} from "react-router-dom";
+
+
+export default function CloseButton({buttonStyle}) {
+    return (<Link to={'/'}>
+        <button className={buttonStyle === 'phoneEnterPageCloseButton' ? `${styles.closeButton} ${styles.phoneEnterPageCloseButton}` : `${styles.closeButton} ${styles.finalInfoPageCloseButton}`}>
+            <div className={styles.closeButtonContent}></div>
+        </button>
+    </Link>);
 }
